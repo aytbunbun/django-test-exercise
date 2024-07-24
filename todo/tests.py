601ca462321eae841361cae0696316ea7711528b
1F10230014 <s1f102300144@iniad.org>
2024-07-24 23:32:14 +0900

    testの修正

--- todo/tests.py
+++ todo/tests.py
@@ -67,9 +67,9 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response. context['tasks']), 1)
 
     def test_index_get_order_post(self):
-        task1 = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
+        task1 = Task.objects.create(title="Task 1", due=timezone.now() + timezone.timedelta(days=1))
         task1.save()
-        task2 = Task(title='task2', due_at=timezone.make_aware(datetime(2024, 8, 1)))
+        task2 = Task.objects.create(title="Task 2", due=timezone.now() + timezone.timedelta(days=2))
         task2.save()
         client = Client()
         response = client. get('/?order=post')
